8cbe86679b6aefd57000765eabc981ef2558e795
renold.dickson <86908465+smarthood@users.noreply.github.com>
2022-03-27 14:47:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const microphoneButton = document.getElementById('microphoneButton');
+
+microphoneButton.addEventListener('click',function(){
+    window.open(
+        './mic/index1.html',
+        "",
+        "width=280px,height=280px"
+    );
+});
+
+
